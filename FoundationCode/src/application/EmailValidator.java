495598a3383a014b28e04d380e929572184ea0d6
Jonathan Waterway <jwaterway@gmail.com>
2025-09-14 22:50:51 -0400

    SCRUM-3: Added EmailValidator stub class

---
+++ FoundationCode/src/application/EmailValidator.java
@@ -0,0 +1,29 @@
+package application;
+
+
+
+public class EmailValidator {
+	
+	
+	 // This class evaluates Email and returns string error messages on invalid input
+	
+	public static String emailErrorMessage = "";		// The error message text
+	public static String passwordInput = "";			// The input being processed
+	private static String inputLine = "";				// The input line
+	private static boolean running;						// The flag that specifies if the FSM is 
+														// running
+	private static int charCounter;						// The index of the current character
+	private static char currentChar;					// The current character in the line
+	  
+	 
+
+	public static String evaluateEmail(String input) {
+	
+		 
+	return emailErrorMessage;
+	
+	}
+	
+	
+
+}
